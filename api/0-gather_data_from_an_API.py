8c3b9e1d7d8a42364c8da834cb45746a901f9736
Alejandro Montenegro <zeituniannicolas27@gmail.com>
2022-12-03 15:38:35 -0300

    try1

---
+++ api/0-gather_data_from_an_API.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""aaaa"""
+import requests
+from sys import argv
+
+if __name__ == '__main__':
+    try:
+        id = int(argv[1])
+    except ValueError:
+        exit()
+
+    user = 'https://jsonplaceholder.typicode.com/users/{}'.format(id)
+    todo = '{}/todos'.format(user)
+
+    res = requests.get(user).json()
+    name = res.get('name')
+    res = requests.get(todo).json()
+    total = len(res)
+    hechont = 0
+
+    for PEPE in res:
+        if PEPE.get('completed') is False:
+            hechont = hechont + 1
+        else:
+            pass
+
+    hecho = total - hechont
+
+    print("Employee {} is done with tasks({}/{}):".format(name, hecho, total))
+
+    for elem in res:
+        if elem.get('completed') is True:
+            print('\t', elem.get('title'))
