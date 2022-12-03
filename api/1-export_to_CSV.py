d36746686dafe2f9e555deda5948a09b822bb21d
Alejandro Montenegro <zeituniannicolas27@gmail.com>
2022-12-03 16:17:30 -0300

    try1 without permissions

---
+++ api/1-export_to_CSV.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""aaaa"""
+import requests
+from sys import argv
+import csv
+
+if __name__ == '__main__':
+    try:
+        id = int(argv[1])
+    except ValueError:
+        exit()
+
+    user = 'https://jsonplaceholder.typicode.com/users/{}'.format(id)
+    todo = "{}/todos".format(user)
+    filename = '{}.csv'.format(id)
+
+    res = requests.get(user).json()
+    username = res.get('username')
+    res = requests.get(todo).json()
+
+    with open(filename, 'w') as csvfile:
+        writer = csv.writer(csvfile, delimiter=',', quoting=csv.QUOTE_ALL)
+
+        for PEPE1 in res:
+            status = PEPE1.get('completed')
+            title = PEPE1.get('title')
+
+            writer.writerow([id, username, status, title])
